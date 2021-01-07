ad345f03bd8e737e7d80ea14d12115d983514d53
qapush <mihail.kapush@gmail.com>
2021-01-07 13:52:42 +0100

    Fetch API instead of XMLHttpRequests

--- src/app.js
+++ src/app.js
@@ -53,57 +53,60 @@ window.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', e => {
 
         e.preventDefault();
-
-        const requestToken = new XMLHttpRequest();
+        
         const c = btoa('c6c984025b5f4c229fcb488d03599144:6760ade5df70467b9a60f73b3f105910');
-        requestToken.open('POST', 'https://accounts.spotify.com/api/token?grant_type=client_credentials');
-        requestToken.setRequestHeader('Authorization', `Basic ${c}`);
-        requestToken.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        requestToken.send();
-        requestToken.addEventListener('load', () => {
 
-            if(requestToken.status === 200){
+        fetch('https://accounts.spotify.com/api/token',{
+            method: 'POST',
+            headers: {
+                Authorization: `Basic ${c}`,
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+            body: "grant_type=client_credentials"
 
-                const requestMain = new XMLHttpRequest();
-                const token = JSON.parse(requestToken.response).access_token;
-                const query = input.value;
-                requestMain.open('GET', `https://api.spotify.com/v1/search?q=${query}&type=playlist`);
-                requestMain.setRequestHeader('Authorization', `Bearer ${token}`);
-                requestMain.send();
-                requestMain.addEventListener('load', () => {
-                   if(requestMain.status === 200){
-                    const results = JSON.parse(requestMain.response).playlists.items;
+        })
+        .then( response => {
+            return response.json();
+        })
+        .then(response => {
+            const token = response.access_token;
+            const query = input.value;
 
-                    result.innerHTML = '';
-                    form.reset();
-                    results.forEach((item) => {
-                        result.innerHTML += `
-                        <div class="result__row">
-                            <div class="result__item">
-                                <div class="result__cover">
-                                    <img src="${item['images'][0]['url']}" alt="">
-                                </div>
-                                <div class="result__description">
-                                    <h2>${item['name']}</h2>
-                                    <hr>
-                                    <div class="item__links">
-                                        <a href="${item['external_urls']['spotify']}" class="link__web" target="_blank">Listen in browser</a> / 
-                                        <a href="${item['uri']}" class="link__app" target="_blank">Listen in Spotify</a>
-                                    </div>
-                                </div>
+            return fetch(`https://api.spotify.com/v1/search?q=${query}&type=playlist`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-type': 'application/x-www-form-urlencoded'
+                }
+            })
+        })
+        .then( response => {
+            return response.json()
+        })
+        .then(response => {
+            const results = response.playlists.items;
+            result.innerHTML = '';
+            form.reset();
+            results.forEach((item) => {
+                result.innerHTML += `
+                <div class="result__row">
+                    <div class="result__item">
+                        <div class="result__cover">
+                            <img src="${item['images'][0]['url']}" alt="">
+                        </div>
+                        <div class="result__description">
+                            <h2>${item['name']}</h2>
+                            <hr>
+                            <div class="item__links">
+                                <a href="${item['external_urls']['spotify']}" class="link__web" target="_blank">Listen in browser</a> / 
+                                <a href="${item['uri']}" class="link__app" target="_blank">Listen in Spotify</a>
                             </div>
                         </div>
-                        `;
-                    });
-
-
-                   }
-                });
-                
-                
-            }
-        });
-        
+                    </div>
+                </div>
+                `;
+            });
+        });       
     })
     
     random();
